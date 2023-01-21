2f0d064201a82a76e14a3a4cef858bbe6de4ae33
AJSmartyPants <81405582+AJSmartyPants@users.noreply.github.com>
2023-01-21 13:27:24 +0530

    Almost done with the home screen UI

--- MedTrack.py
+++ MedTrack.py
@@ -12,13 +12,20 @@ root.state('zoomed')
 
 #Load the images
 
-# Open the JPG image file
 aicon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\MedTrackAppIcon.png").resize((70, 70), Image.LANCZOS)
-# Create a PhotoImage object from the JPG image
 appicon = ImageTk.PhotoImage(aicon)
 
-sicon = PhotoImage(file = r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\ScanIcon.png")
-scanicon = sicon.subsample(3, 3)
+sicon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\ScanIcon.png").resize((150, 150), Image.LANCZOS)
+scanicon = ImageTk.PhotoImage(sicon)
+
+iicon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\InfoLibIcon.png").resize((150, 150), Image.LANCZOS)
+infoicon = ImageTk.PhotoImage(iicon)
+
+cicon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\ChatIcon.png").resize((150, 150), Image.LANCZOS)
+chaticon = ImageTk.PhotoImage(cicon)
+
+micon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\MedIcon.png").resize((150, 150), Image.LANCZOS)
+medicon = ImageTk.PhotoImage(micon)
 
 #Create the functions
 
@@ -29,13 +36,13 @@ scanicon = sicon.subsample(3, 3)
 appiconimg = Label(root, image = appicon)
 title = Label(root, text = "Welcome to\rMedTrack!", font = ('Berlin Sans FB Demi', 30, 'bold'), fg = '#ff4d00', bg = 'white')
 hsb = Button(root, text = 'Scan', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
-             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+             activebackground = '#99FFFF', height = 250, image = scanicon, compound = BOTTOM)
 hib = Button(root, text = 'Information Library', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
-             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+             activebackground = '#99FFFF', height = 250, image = infoicon, compound = BOTTOM)
 hcb = Button(root, text = 'Chatbot', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
-             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+             activebackground = '#99FFFF', height = 250, image = chaticon, compound = BOTTOM)
 hmb = Button(root, text = 'Medicines', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
-             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+             activebackground = '#99FFFF', height = 250, image = medicon, compound = BOTTOM)
 
 appiconimg.grid(row = 0, column = 0, sticky = 'w')
 title.grid(row = 0, column = 1, sticky = 'w')
