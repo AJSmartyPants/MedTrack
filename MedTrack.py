832f59f7c68909c219688d87057eadace64428fb
AJSmartyPants <81405582+AJSmartyPants@users.noreply.github.com>
2023-01-21 12:28:07 +0530

    Updated my code before lunch

--- MedTrack.py
+++ MedTrack.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from tkinter.ttk import *
+from PIL import Image, ImageTk
 import cv2
 import pytesseract
 import webview
@@ -12,9 +12,12 @@ root.state('zoomed')
 
 #Load the images
 
-aicon = PhotoImage(file = r"C:\Users\tinao\OneDrive\Desktop\MedTrack\Images\MedTrackAppIcon.png")
-appicon = aicon.subsample(2, 2)
-sicon = PhotoImage(file = r"C:\Users\tinao\OneDrive\Desktop\MedTrack\Images\ScanIcon.png")
+# Open the JPG image file
+aicon = Image.open(r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\MedTrackAppIcon.png").resize((70, 70), Image.LANCZOS)
+# Create a PhotoImage object from the JPG image
+appicon = ImageTk.PhotoImage(aicon)
+
+sicon = PhotoImage(file = r"C:\Users\tinao\OneDrive\Desktop\MedTrack\MedTrackGithub\Images\ScanIcon.png")
 scanicon = sicon.subsample(3, 3)
 
 #Create the functions
@@ -23,11 +26,22 @@ scanicon = sicon.subsample(3, 3)
 
 #Create the widgets and add functionality
 
-title = Label(root, text = "Welcome to MedTrack!", font = ('Berlin Sans FB Demi', 30, 'bold'), fg = '#ff4d00', bg = 'white')
+appiconimg = Label(root, image = appicon)
+title = Label(root, text = "Welcome to\rMedTrack!", font = ('Berlin Sans FB Demi', 30, 'bold'), fg = '#ff4d00', bg = 'white')
 hsb = Button(root, text = 'Scan', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
              activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+hib = Button(root, text = 'Information Library', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
+             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+hcb = Button(root, text = 'Chatbot', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
+             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
+hmb = Button(root, text = 'Medicines', font=('Britannic Bold', 20, 'bold'), fg = '#7700CC', activeforeground = '#0099FF', bg = '#9999FF',
+             activebackground = '#99FFFF', image = scanicon, compound = BOTTOM)
 
-title.grid(row = 0, column = 0)
-hsb.grid(row = 1, column = 0)
+appiconimg.grid(row = 0, column = 0, sticky = 'w')
+title.grid(row = 0, column = 1, sticky = 'w')
+hsb.grid(row = 1, column = 0, sticky = 'w', pady = 20)
+hib.grid(row = 1, column = 1, sticky = 'w', padx = 10, pady = 20)
+hcb.grid(row = 1, column = 2, sticky = 'w', padx = 10, pady = 20)
+hmb.grid(row = 1, column = 3, sticky = 'w', padx = 10, pady = 20)
 mainloop()
 
